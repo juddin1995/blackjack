215987cbd7e1bb5b1d311bb45ea784ee237a98d7
jomir <jomir.uddin112@gmail.com>
2024-08-01 17:07:42 -0400

    Complete Win/Lose Logic

--- blackjack.js
+++ blackjack.js
@@ -9,19 +9,37 @@ let deck;
 
 let canHit = true; // player can draw while playerSum <= 21
 
-window.onload = function() {
+let playerBankroll = 1000; // Starting bankroll
+let currentBet = 0; // Current bet amount
+
+window.onload = function () {
   buildDeck();
   shuffleDeck();
   startGame();
-}
+  document.getElementById("place-bet").addEventListener("click", placeBet);
+};
 
 function buildDeck() {
-  let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  let types = ['C', 'D', 'H', 'S'];
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let types = ["C", "D", "H", "S"];
   deck = [];
 
-  for(let i = 0; i < types.length; i++) {
-    for(let j = 0; j < values.length; j++) {
+  for (let i = 0; i < types.length; i++) {
+    for (let j = 0; j < values.length; j++) {
       deck.push(`${values[j]}-${types[i]}`);
       // deck.push(values[j] + "-" + types[i]);
     }
@@ -55,7 +73,7 @@ function startGame() {
     document.getElementById("dealer-cards").append(cardImg);
   }
   console.log(dealerSum);
-  
+
   for (let i = 0; i < 2; i++) {
     let cardImg = document.createElement("img");
     let card = deck.pop();
@@ -65,6 +83,48 @@ function startGame() {
     document.getElementById("player-cards").append(cardImg);
   }
   console.log(playerSum);
+  document.getElementById("hit").addEventListener("click", hit);
+  document.getElementById("stay").addEventListener("click", stay);
+}
+
+function hit() {
+  if (!canHit) {
+    return;
+  }
+
+  let cardImg = document.createElement("img");
+  let card = deck.pop();
+  cardImg.src = `./cards/${card}.png`;
+  playerSum += getValue(card);
+  playerAceCount += checkAce(card);
+  document.getElementById("player-cards").append(cardImg);
+
+  if (reduceAce(playerSum, playerAceCount) > 21) {
+    canHit = false;
+  }
+}
+
+function stay() {
+  dealerSum = reduceAce(dealerSum, dealerAceCount);
+  playerSum = reduceAce(playerSum, playerAceCount);
+
+  canHit = false;
+  document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+
+  let message = "";
+  if (playerSum > 21) {
+    message = "You Lose!";
+  } else if (dealerSum > 21) {
+    message = "You Win!";
+  } else if (playerSum === dealerSum) {
+    message = "Tie!";
+  } else if (playerSum < dealerSum) {
+    message = "You Lose!";
+  }
+
+  document.getElementById("dealer-sum").innerText = dealerSum;
+  document.getElementById("player-sum").innerText = playerSum;
+  document.getElementById("results").innerText = message;
 }
 
 function getValue(card) {
@@ -74,15 +134,23 @@ function getValue(card) {
   if (isNaN(value)) {
     if (value === "A") {
       return 11;
-    } 
+    }
     return 10;
   }
   return parseInt(value);
 }
 
 function checkAce(card) {
-  if (card[0] === 'A') {
+  if (card[0] === "A") {
     return 1;
   }
   return 0;
+}
+
+function reduceAce(playerSum, playerAceCount) {
+  while (playerSum > 21 && playerAceCount > 0) {
+    playerSum -= 10;
+    playerAceCount -= 1;
+  }
+  return playerSum;
 }
